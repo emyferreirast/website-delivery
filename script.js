99b80c5419aa087dd4e2e97fa84f3bb016558547
Emelly Ferreira <emelly.dev@outlook.com>
2025-05-23 21:32:37 -0300

    finalizado

--- script.js
+++ script.js
@@ -130,4 +130,24 @@ function removeItemCart(name){
    
 }
 
+//endereço
+
+addressInput.addEventListener("input", function(event){
+    let inputValue = event.target.value;
+
+    if(inputValue !== ""){
+        addressInput.classList.remove("border-red-500")
+        addressWarn.classList.add("hidden")
+    }
+})
+
+checkoutBtn.addEventListener("click", function (){
+    if(cart.length === 0) return;
+    if(addressInput.value === ""){
+        addressWarn.classList.remove("hidden")
+        addressInput.classList.add("border-red-500")
+        return;
+    }
+})
+
 
